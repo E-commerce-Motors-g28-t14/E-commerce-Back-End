bf5a227d17ea8d5bff259ad9884e4049336bfb07
Vinicius Pinheiro <viniciusps_1998@hotmail.com>
2023-06-07 17:09:44 -0300

    feat: update car with photos

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -10,7 +10,7 @@ const carRouter: Router = Router();
 
 carRouter.post("", CreateCarController);
 carRouter.get("", GetCarsController);
-carRouter.patch("/:id", UpdateCarController);
+carRouter.put("/:id", UpdateCarController);
 carRouter.delete("/:id", RemoveCarController);
 
 export default carRouter;
--- src/services/cars/UpdateCar.service.ts
+++ src/services/cars/UpdateCar.service.ts
@@ -1,23 +1,51 @@
 import { Car } from "../../entities";
-import { ICarResponse, ICarUpdate } from "../../interfaces";
-import { carRepository } from "../../repositories";
+import { ICarResponse, ICarUpdate, IPhotoResponse } from "../../interfaces";
+import { carRepository, photoRepository } from "../../repositories";
 import { carResponseSerializer } from "../../serializers";
 
 export const UpdateCarService = async (
   carData: ICarUpdate,
   carId: string
 ): Promise<ICarResponse> => {
-  const { newPhotos, ...payload } = carData;
+  const { newPhotos, fipePrice, ...payload } = carData;
 
-  await carRepository.update(carId, payload);
+  const isInPromo: boolean = +fipePrice * 0.95 >= +payload.price;
 
-  const car: Car = await carRepository
+  const car: Car = await carRepository.findOneBy({ id: carId });
+
+  await carRepository.update(carId, {
+    ...payload,
+    isPromo: isInPromo,
+  });
+
+  const photos = await photoRepository.find({
+    where: {
+      car: {
+        id: carId,
+      },
+    },
+  });
+
+  await photoRepository.remove(photos);
+
+  await Promise.all(
+    newPhotos.map(async (photo) => {
+      const photoData: IPhotoResponse = photoRepository.create({
+        ...photo,
+        car,
+      });
+
+      await photoRepository.save(photoData);
+    })
+  );
+
+  const updatedCar: Car = await carRepository
     .createQueryBuilder("car")
     .where("car.id = :id", { id: carId })
     .leftJoinAndSelect("car.photos", "photos")
     .getOne();
 
-  const carReturn: ICarResponse = carResponseSerializer.parse(car);
+  const carReturn: ICarResponse = carResponseSerializer.parse(updatedCar);
 
   return carReturn;
 };
